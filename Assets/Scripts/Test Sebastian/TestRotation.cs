6424312865f094ba259ebd9b4d74e90658da4c0d
sammx343 <naitsabes29@gmail.com>
2021-06-02 21:01:35 -0500

    Fixed Bull Movement on Test

---
+++ Assets/Scripts/Test Sebastian/TestRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] 
+    Transform pointer;
+
+    private float angle;
+    public float speed = 5;
+    private Vector3 Vec;
+    public float turnFactor = 5;
+    private TopDownCarController _topDownCarController;
+    void Start()
+    {
+        angle = 0;
+        Vec = Vector3.zero;
+        _topDownCarController = GetComponent<TopDownCarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+        if (Physics.Raycast(ray, out _hit, Mathf.Infinity))
+        {
+            pointer.transform.position = new Vector3(_hit.point.x, 0.1f, _hit.point.z);
+        }
+
+        Vector3 direction = pointer.transform.position - transform.position;
+        angle = Mathf.LerpAngle(angle, Vector3.SignedAngle(direction.normalized, transform.forward, Vector3.up), Time.deltaTime * speed)%360;
+        Debug.Log(-angle);
+        //
+        // transform.rotation =  Quaternion.Euler(0,angle, 0);
+         
+        // Vec.x = Input.GetAxis("Horizontal");  
+        Vec.x = (-angle/360) * turnFactor; 
+        Vec.z = 1;  
+        _topDownCarController.SetInputVector(new Vector2(Vec.x, Vec.z));
+    }
+}
---
+++ Assets/Scripts/Test Sebastian/TopDownCarController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCarController : MonoBehaviour
+{
+    [Header("Car settings")]
+    public float driftFactor = 0.95f;
+    public float accelerationFactor = 30.0f;
+    public float turnFactor = 3.5f;
+    public float maxSpeed = 20;
+
+    //Local variables
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+
+    float velocityVsUp = 0;
+
+    //Components
+    Rigidbody carRigidbody;
+
+    //Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        carRigidbody = GetComponent<Rigidbody>();
+    }
+
+    //Frame-rate independent for physics calculations.
+    void FixedUpdate()
+    {
+        ApplyEngineForce();
+
+        KillOrthogonalVelocity();
+
+        ApplySteering();
+    }
+
+    void ApplyEngineForce()
+    {
+        //Apply drag if there is no accelerationInput so the car stops when the player lets go of the accelerator
+        if (accelerationInput == 0)
+            carRigidbody.drag = Mathf.Lerp(carRigidbody.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else carRigidbody.drag = 0;
+
+        //Caculate how much "forward" we are going in terms of the direction of our velocity
+        velocityVsUp = Vector3.Dot(transform.forward, carRigidbody.velocity);
+
+        //Limit so we cannot go faster than the max speed in the "forward" direction
+        if (velocityVsUp > maxSpeed && accelerationInput > 0)
+            return;
+
+        //Limit so we cannot go faster than the 50% of max speed in the "reverse" direction
+        if (velocityVsUp < -maxSpeed*0.5f && accelerationInput < 0)
+            return;
+
+        //Limit so we cannot go faster in any direction while accelerating
+        if (carRigidbody.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+            return;
+
+        //Create a force for the engine
+        Vector3 engineForceVector = transform.forward * accelerationInput * accelerationFactor;
+        
+        //Apply force and pushes the car forward
+        carRigidbody.AddForce(engineForceVector, ForceMode.Force);
+    }
+
+    void ApplySteering()
+    {
+        //Limit the cars ability to turn when moving slowly
+        float minSpeedBeforeAllowTurningFactor = (carRigidbody.velocity.magnitude / 2);
+        minSpeedBeforeAllowTurningFactor = Mathf.Clamp01(minSpeedBeforeAllowTurningFactor);
+
+        //Update the rotation angle based on input
+        rotationAngle -= steeringInput * turnFactor * minSpeedBeforeAllowTurningFactor;
+
+        //Apply steering by rotating the car object
+        carRigidbody.MoveRotation(Quaternion.Euler(0, rotationAngle, 0));
+    }
+
+    void KillOrthogonalVelocity()
+    {
+        //Get forward and right velocity of the car
+        Vector3 forwardVelocity = transform.forward * Vector3.Dot(carRigidbody.velocity, transform.forward);
+        Vector3 rightVelocity = transform.right * Vector3.Dot(carRigidbody.velocity, transform.right);
+
+        //Kill the orthogonal velocity (side velocity) based on how much the car should drift. 
+        carRigidbody.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+    float GetLateralVelocity()
+    {
+        //Returns how how fast the car is moving sideways. 
+        return Vector3.Dot(transform.right, carRigidbody.velocity);
+    }
+
+    public bool IsTireScreeching(out float lateralVelocity, out bool isBraking)
+    {
+        lateralVelocity = GetLateralVelocity();
+        isBraking = false;
+
+        //Check if we are moving forward and if the player is hitting the brakes. In that case the tires should screech.
+        if (accelerationInput < 0 && velocityVsUp > 0)
+        {
+            isBraking = true;
+            return true;
+        }
+
+        //If we have a lot of side movement then the tires should be screeching
+        if (Mathf.Abs(GetLateralVelocity()) > 4.0f)
+            return true;
+
+        return false;
+    }
+
+    public void SetInputVector(Vector2 inputVector)
+    {
+        steeringInput = -inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+
+    public float GetVelocityMagnitude()
+    {
+        return carRigidbody.velocity.magnitude;
+    }
+}
